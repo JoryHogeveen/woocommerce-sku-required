0352bdaab3dc331479d8bd4300b6731e2f88d226
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2020-08-28 17:53:20 +0200

    v2.0
    - Checkbox for admins to make SKU optional
    - Properly validate variations

--- js/script.js
+++ js/script.js
@@ -1,60 +1,178 @@
 /**
  * @author Jory Hogeveen
- * @version 1.0.1
+ * @version 2.0
  */
 
 if ( 'undefined' === typeof wooSkuRequired ) {
 	wooSkuRequired = {
+		_debug: false,
+		ajax_url: '/wp-admin/admin-ajax.php',
+		load_variations_nonce: '',
+		product_id: 0,
 		__required: 'SKU field is required',
 	};
 }
 
 jQuery( function( $ ) {
-	wooSkuRequired.form   = $( 'form#post' );
-	wooSkuRequired.errors = [];
+	wooSkuRequired.$form         = $( 'form#post' );
+	wooSkuRequired.errors        = [];
+	wooSkuRequired.$error_target = null;
+	wooSkuRequired.variations    = {};
 
-	wooSkuRequired.form.on( 'submit', function( e ) {
+	// Save product.
+	wooSkuRequired.$form.on( 'submit', function( e ) {
+		wooSkuRequired.disableSubmit();
+
+		wooSkuRequired.$error_target = wooSkuRequired.$form;
+
 		if ( true === wooSkuRequired.validate_sku() ) {
+			wooSkuRequired.enableSubmit();
 			return true;
 		}
 		e.preventDefault();
+		e.stopPropagation();
+
+		wooSkuRequired.enableSubmit();
 		return false;
 	} );
+
+	// Save variations.
+	$( 'button.save-variation-changes' ).on( 'click', function( e ) {
+		wooSkuRequired.disableSubmit();
+
+		// Only validate loaded variations.
+		wooSkuRequired.variations = {};
+		wooSkuRequired.$form.find( 'input[id^=variable_sku]' ).each( function() {
+			var $this = $( this ),
+				id = $this.parents( '.woocommerce_variation' ).find( 'input[name^=variable_post_id]' ).val();
+			wooSkuRequired.variations[ id ] = $this.val();
+		} );
+
+		wooSkuRequired.$error_target = $( '#variable_product_options_inner .toolbar-top' );
+
+		if ( true === wooSkuRequired.validate_sku() ) {
+			wooSkuRequired.enableSubmit();
+			return true;
+		}
+		e.preventDefault();
+		e.stopPropagation();
+
+		wooSkuRequired.enableSubmit();
+		return false;
+	} );
+
+	// Reload variations.
+	$( '#woocommerce-product-data' ).on( 'woocommerce_variations_saved', function () {
+		wooSkuRequired.load_variations();
+	} );
+
+	wooSkuRequired.disableSubmit = function() {
+		$( '#submitpost input, button.save-variation-changes' ).each( function() {
+			var $this = $( this ),
+				$spinner = $this.parent().find( '.spinner' );
+
+			$this.prop( 'disabled', true );
+			if ( ! $spinner.length ) {
+				$this.before( '<span class="spinner"></span>' );
+				$spinner = $this.parent().find( '.spinner' );
+			}
+			$spinner.addClass( 'is-active' );
+		} );
+	}
+
+	wooSkuRequired.enableSubmit = function() {
+		$( '#submitpost input, button.save-variation-changes' ).each( function() {
+			var $this = $( this ),
+				$spinner = $this.parent().find( '.spinner' );
+
+			$this.prop( 'disabled', false );
+			if ( ! $spinner.length ) {
+				$this.before( '<span class="spinner"></span>' );
+				$spinner = $this.parent().find( '.spinner' );
+			}
+			$spinner.removeClass( 'is-active' );
+		} );
+	}
+
+	/**
+	 * @param {object} e Event.
+	 * @param {object} $submit Submit button jQuery element.
+	 * @param {object} $spinner Spinner jQuery element.
+	 * @returns {boolean}
+	 */
+	wooSkuRequired.handleSubmit = function ( e, $submit, $spinner ) {
+		$spinner.addClass( 'is-active' );
+		$submit.addClass( 'disabled' );
+		if ( true === wooSkuRequired.validate_sku() ) {
+			$spinner.removeClass( 'is-active' );
+			$submit.removeClass( 'disabled' );
+			return true;
+		}
+		$spinner.removeClass( 'is-active' );
+		$submit.removeClass( 'disabled' );
+		e.preventDefault();
+		e.stopPropagation();
+		return false;
+	}
+
+	/**
+	 * @return {boolean}
+	 */
+	wooSkuRequired.is_required = function () {
+		var $disable_sku  = wooSkuRequired.$form.find( 'input#sku_optional' );
+
+		if ( ! $disable_sku.length ) {
+			return true;
+		}
+
+		if ( 'hidden' === $disable_sku.attr( 'type' ) ) {
+			return ! Boolean( $disable_sku.val() );
+		}
+
+		return ! $disable_sku.is(':checked');
+	}
 
 	/**
 	 * @return {boolean}
 	 */
 	wooSkuRequired.validate_sku = function() {
-		var $main_sku     = wooSkuRequired.form.find( 'input#_sku' ),
-			$variable_sku = wooSkuRequired.form.find( 'input[id^=variable_sku]' ),
-			product_type  = wooSkuRequired.form.find( '#product-type' ).val();
+		var main_sku     = wooSkuRequired.$form.find( 'input#_sku' ).val(),
+			product_type = wooSkuRequired.$form.find( '#product-type' ).val();
+
+		if ( ! wooSkuRequired.is_required() ) {
+			wooSkuRequired.debug( 'SKU not required' );
+			return true;
+		}
 
 		wooSkuRequired.clear_errors();
 
-		if ( $variable_sku.length || 'variable' === product_type ) {
+		if ( main_sku ) {
+			wooSkuRequired.debug( 'Main SKU found: ' + main_sku );
+			return true;
+		}
+
+		if ( 'variable' === product_type ) {
 			// Variable product.
-			if ( ! $main_sku.val().length ) {
-				// No main SKU, variable SKU required.
-				$.each( $variable_sku, function ( index, element ) {
-					var $element = $( element );
-					if ( ! $element.val().length ) {
-						var id = $element.parents( '.woocommerce_variation' ).find( 'input[name^=variable_post_id]' ).val();
-						wooSkuRequired.errors.push( {
-							id: 'sku_' + id,
-							message: '#' + id + ': ' + wooSkuRequired.__required
-						} );
-					}
-				} );
-			}
+			$.each( wooSkuRequired.variations, function ( id, sku ) {
+				if ( ! sku.length ) {
+					wooSkuRequired.errors.push( {
+						id: 'sku_' + id,
+						message: '#' + id + ': ' + wooSkuRequired.__required
+					} );
+				}
+			} );
+
+			wooSkuRequired.debug( wooSkuRequired.variations );
+
 		} else {
 			// Simple product.
-			if ( ! $main_sku.val().length ) {
-				wooSkuRequired.errors.push( {
-					id: 'sku_0',
-					message: wooSkuRequired.__required
-				} );
-			}
+			wooSkuRequired.errors.push( {
+				id: 'sku_0',
+				message: wooSkuRequired.__required
+			} );
 		}
+
+		wooSkuRequired.debug( wooSkuRequired.errors );
 
 		if ( wooSkuRequired.errors.length ) {
 			wooSkuRequired.show_errors();
@@ -64,10 +182,9 @@ jQuery( function( $ ) {
 	}
 
 	/**
-	 * @param {object} error
 	 * @return void
 	 */
-	wooSkuRequired.clear_errors = function( error ) {
+	wooSkuRequired.clear_errors = function() {
 		$.each ( wooSkuRequired.errors, function( index, error ) {
 			wooSkuRequired.remove_error( error.id );
 		} );
@@ -89,12 +206,49 @@ jQuery( function( $ ) {
 		$.each ( wooSkuRequired.errors, function( index, error ) {
 			error.message += '<button type="button" class="notice-dismiss"></button>';
 			error.message = '<div id="' + error.id + '" class="notice error is-dismissible"><p>' + error.message + '</p></div>';
-			wooSkuRequired.form.before( error.message );
+			wooSkuRequired.$error_target.before( error.message );
 
 			$( '#' + error.id + ' .notice-dismiss' ).on( 'click', function() {
 				wooSkuRequired.remove_error( error.id );
 			} );
 		} );
 	}
+
+	/**
+	 * Load variation SKU.
+	 */
+	wooSkuRequired.load_variations = function() {
+		wooSkuRequired.disableSubmit();
+		$.ajax({
+			//async: false,
+			url: wooSkuRequired.ajax_url,
+			data: {
+				action:     'load_variations_sku',
+				_nonce:     wooSkuRequired.load_variations_nonce,
+				product_id: wooSkuRequired.product_id
+			},
+			type: 'POST',
+			success: function( response ) {
+				if ( response.hasOwnProperty( 'data' ) && response.hasOwnProperty( 'success' ) ) {
+					if ( response.success ) {
+						wooSkuRequired.variations = response.data;
+					}
+				}
+				wooSkuRequired.debug( wooSkuRequired.variations );
+				wooSkuRequired.enableSubmit();
+			}
+		});
+	}
+
+	/**
+	 * @param {mixed} value
+	 */
+	wooSkuRequired.debug = function ( value ) {
+		if ( wooSkuRequired._debug ) {
+			console.log( value );
+		}
+	}
+
+	wooSkuRequired.load_variations();
 
 } );
--- woocommerce-sku-required.php
+++ woocommerce-sku-required.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WooCommerce SKU Required
  * Description: Make the WooCommerce SKU field required.
- * Version:     1.0.1
+ * Version:     2.0
  * Author:      Jory Hogeveen
  * Author URI:  https://www.keraweb.nl
  * Text Domain: woocommerce-sku-required
@@ -21,7 +21,17 @@ class WooCommerce_SKU_Required
 	/**
 	 * @var float
 	 */
-	const VERSION = '1.0';
+	const VERSION = '2.0';
+
+	/**
+	 * @var string
+	 */
+	public $meta_optional = 'sku_optional';
+
+	/**
+	 * @var string
+	 */
+	public $capability = 'manage_woocommerce';
 
 	/**
 	 * @var WooCommerce_SKU_Required
@@ -42,7 +52,23 @@ class WooCommerce_SKU_Required
 	 * WooCommerce_SKU_Required constructor.
 	 */
 	protected function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
+	}
+
+	/**
+	 * WP initialized.
+	 */
+	public function init() {
+		if ( ! wc_product_sku_enabled() ) {
+			return;
+		}
+
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
+
+		add_action( 'woocommerce_product_options_sku', array( $this, 'action_woocommerce_product_options_sku' ) );
+		add_action( 'woocommerce_process_product_meta', array( $this, 'action_woocommerce_process_product_meta' ) );
+
+		add_action( 'wp_ajax_load_variations_sku', array( $this, 'ajax_load_variations_sku' ) );
 	}
 
 	/**
@@ -61,8 +87,85 @@ class WooCommerce_SKU_Required
 		wp_enqueue_script( 'woocommerce-sku-required', $url . 'js/script.js', array( 'jquery' ), self::VERSION, true );
 
 		$l10n = array(
-			'__required' => __( 'SKU field is required', 'woocommerce-sku-required' ),
+			'_debug'                => WP_DEBUG,
+			'ajax_url'              => admin_url( 'admin-ajax.php' ),
+			'load_variations_nonce' => wp_create_nonce( 'load-variations-sku' ),
+			'product_id'            => wc_get_product()->get_id(),
+			'__required'            => __( 'SKU field is required', 'woocommerce-sku-required' ),
 		);
 		wp_localize_script( 'woocommerce-sku-required', 'wooSkuRequired', $l10n );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function ajax_load_variations_sku() {
+
+		check_ajax_referer( 'load-variations-sku', '_nonce' );
+
+		if ( ! current_user_can( 'edit_products' ) || empty( $_POST['product_id'] ) ) {
+			wp_send_json_error();
+			wp_die( -1 );
+		}
+
+		$product_id = absint( $_POST['product_id'] );
+		$variations = wc_get_products(
+			array(
+				'status'  => array( 'private', 'publish' ),
+				'type'    => 'variation',
+				'parent'  => $product_id,
+				'limit'   => -1,
+				'orderby' => array(
+					'menu_order' => 'ASC',
+					'ID'         => 'DESC',
+				),
+				'return'  => 'objects',
+			)
+		);
+
+		$return = array();
+
+		foreach ( $variations as $variation ) {
+			$variation = wc_get_product( $variation );
+			// Pass `raw` as context to prevent loading parent SKU.
+			$return[ $variation->get_id() ] = $variation->get_sku( 'raw' );
+		}
+
+		wp_send_json_success( $return );
+	}
+
+	/**
+	 * Add SKU fields.
+	 */
+	public function action_woocommerce_product_options_sku() {
+		$product = wc_get_product();
+		$key     = $this->meta_optional;
+
+		if ( current_user_can( $this->capability ) ) {
+			woocommerce_wp_checkbox(
+				array(
+					'id'            => $key,
+					'value'         => get_post_meta( $product->get_id(), $key, true ) ? 'yes' : 'no',
+					'label'         => __( 'Optional SKU?', 'woocommerce-sku-required' ),
+					'description'   => __( 'SKU is optional for this product (and variations)', 'woocommerce-sku-required' ),
+				)
+			);
+		} else {
+			echo '<input type="hidden" name="' . $key . '" id="' . $key . '" value="' . (bool) $product->get_meta( $key ) . '" disabled="disabled" />';
+		}
+	}
+
+	/**
+	 * @param int $post_id
+	 */
+	public function action_woocommerce_process_product_meta( $post_id ) {
+		if ( ! current_user_can( $this->capability ) ) {
+			return;
+		}
+		$product = wc_get_product( $post_id );
+
+		$value = ( ! empty( $_POST[ $this->meta_optional ] ) && 'yes' === $_POST[ $this->meta_optional ] );
+
+		update_post_meta( $product->get_id(), $this->meta_optional, (bool) $value );
 	}
 }
